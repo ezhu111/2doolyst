20ab3ea78e35f243c6020e5836de1dd4686f0805
ezhu111 <77119733+ezhu111@users.noreply.github.com>
2021-12-04 12:16:44 -0700

    added google and github provider

--- components/authentication/providers/github/GitHubProvider.js
+++ components/authentication/providers/github/GitHubProvider.js
@@ -1,25 +1,46 @@
 import Image from "next/image";
-
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { GithubAuthProvider, signInWithPopup } from "@firebase/auth";
+import { auth } from "libs/firebase";
+import { useAuth } from "libs/hooks/useAuth";
 import { ProviderButton } from "ui/buttons";
 
 import github from "./github.png";
 
 function GitHubProvider({ children, ...props }) {
-  return (
-    <ProviderButton>
-      <div>
-        <Image
-          src={github}
-          layout="fixed"
-          alt="github"
-          width={24}
-          height={24}
-          quality={30}
-        />
-        <span> {children}</span>
-      </div>
-    </ProviderButton>
-  );
+    const [isValidUser, setIsValidUser] = useState(null)
+    const user = useAuth();
+    const router = useRouter()
+    const provider = new GithubAuthProvider();
+
+    async function signIn(){
+        setIsValidUser(await signInWithPopup(auth, provider))
+    }
+
+    function handleClick() {
+        signIn()
+    }
+
+    if(isValidUser){
+        router.push('/todo')
+    }
+
+    return (
+        <ProviderButton onClick={(e)=>handleClick(e)} {...props}>
+            <div>
+                <Image
+                    src={github}
+                    layout="fixed"
+                    alt="github"
+                    width={24}
+                    height={24}
+                    quality={30}
+                />
+                <span> {children}</span>
+            </div>
+        </ProviderButton>
+    );
 }
 
 export default GitHubProvider;
--- components/authentication/providers/google/GoogleProvider.js
+++ components/authentication/providers/google/GoogleProvider.js
@@ -1,25 +1,45 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { GoogleAuthProvider, signInWithPopup } from '@firebase/auth';
+import { auth } from 'libs/firebase';
+import { useAuth } from 'libs/hooks/useAuth';
 import { ProviderButton } from "ui/buttons";
 import google from "./google.png";
 
 function GoogleProvider({ children,  ...props }) {
- 
+    const [isValidUser, setIsValidUser] = useState(null)
+    const user = useAuth();
+    const router = useRouter();
+    const provider = new GoogleAuthProvider();
 
-  return (
-    <ProviderButton >
-      <div>
-        <Image
-          src={google}
-          layout="fixed"
-          alt="google"
-          width={24}
-          height={24}
-          quality={30}
-        />
-        <span>{children}</span>
-      </div>
-    </ProviderButton>
-  );
+    async function signIn(){
+        setIsValidUser(await signInWithPopup(auth, provider))
+    }
+
+    function handleClick() {
+        signIn()
+    }
+
+    if(isValidUser){
+        router.push('/todo')
+    }
+
+    return (
+        <ProviderButton onClick={handleClick} {...props}>
+            <div>
+                <Image
+                    src={google}
+                    layout="fixed"
+                    alt="google"
+                    width={24}
+                    height={24}
+                    quality={30}
+                />
+                <span>{children}</span>
+            </div>
+        </ProviderButton>
+    );
 }
 
 export default GoogleProvider;
